b944c0640770150fa08623e3fb67eeb1921d1830
junaidiqbalpg <junaidiqbalpg@gmail.com>
2021-03-27 16:26:09 +0000

    Add functionality for ActorRepository and update Unit test for it.

--- GitEventTrackingApi/GitEventTrackingApi.Test/Respository/ActorRespositoryTests.cs
+++ GitEventTrackingApi/GitEventTrackingApi.Test/Respository/ActorRespositoryTests.cs
@@ -1,5 +1,10 @@
-﻿using GitEventTrackingApi.Data.Domain;
+﻿using AutoMapper;
+using GitEventTrackingApi.Data;
+using GitEventTrackingApi.Data.Domain;
 using GitEventTrackingApi.Data.Repository;
+using GitEventTrackingApi.Test.Helper;
+using Microsoft.Extensions.Logging;
+using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -11,23 +16,29 @@ namespace GitEventTrackingApi.Test.Respository
     {
         private ActorRepository _actorRepository;
 
+        private Mock<ILogger<ActorRepository>> _actorRepositoryLogger;
+
+        private GitEventTrackingContext _context = new SqliteDbContext().CreateContext();
+
         [SetUp]
         public void Init()
         {
-            _actorRepository = new ActorRepository();
+            _actorRepositoryLogger = new Mock<ILogger<ActorRepository>>();
+
+            _actorRepository = new ActorRepository(_context, _actorRepositoryLogger.Object);
         }
 
         [Test]
         public void ReturnAllActors()
         {
             //Arrange
-            List<Actor> actors = new List<Actor>();
+            int expectedActors = 0;
 
             //Act
             var result = _actorRepository.GetAllActors();
 
             //Assert
-            Assert.That(result.Count, Is.EqualTo(actors.Count));
+            Assert.That(result.Count, Is.GreaterThanOrEqualTo(expectedActors));
         }
     }
 }
